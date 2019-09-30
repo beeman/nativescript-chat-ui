937b26a1785a701f50a67ef94b1a65e9294a1e07
Bram Borggreve <borggreve@gmail.com>
2019-09-29 22:35:39 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { isAndroid } from "tns-core-modules/platform";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html",
+    styleUrls: ["./app.component.scss"]
+})
+export class AppComponent implements OnInit {
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { DataService, IDataItem } from "../shared/data.service";
+
+@Component({
+    selector: "Home",
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent implements OnInit {
+    items: Array<IDataItem>;
+
+    constructor(private _itemService: DataService) { }
+
+    ngOnInit(): void {
+        this.items = this._itemService.getItems();
+    }
+}
